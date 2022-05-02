b68cf65d3d299a31ac703f948b4e388619da6128
Dboi <dboi@buntuvpn.domain>
2022-05-02 19:34:03 +0000

    make it better

--- MongoDB.js
+++ MongoDB.js
@@ -1,100 +1,94 @@
-const DATABASE_URL = require('dotenv').config()
+const DATABASE_URL = require("dotenv").config();
 //create the client class
-const { MongoClient } = require("mongodb")
+const { MongoClient } = require("mongodb");
 // URI for Mongo database
-const uri = process.env.DATABASE_URL
+const uri = process.env.DATABASE_URL;
 
 // Query Mongo DB for movie details
 async function dbSearch(searchTerm) {
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
-    try {
-        await client.connect();
-        const db = client.db("MoviesDB");
-        console.log(`Connected to database ${db.databaseName}`)
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  try {
+    await client.connect();
+    const db = client.db("MoviesDB");
+    console.log(`Connected to database ${db.databaseName}`);
 
-        const movies = db.collection("Movies");
-        const movieslist = await movies.find({ "title": { $regex: searchTerm, $options: "$is" } })
-        return await movieslist.toArray()
-    }
-    catch (ex) {
-        console.error(`Something bad happend ${ex}`)
-    }
-    finally {
-        client.close();
-    }
+    const movies = db.collection("Movies");
+    const movieslist = await movies.find({
+      title: { $regex: searchTerm, $options: "$is" },
+    });
+    return await movieslist.toArray();
+  } catch (ex) {
+    console.error(`Something bad happend ${ex}`);
+  } finally {
+    client.close();
+  }
 }
 // add comment to specified movie
 async function dbAddComment(movie, comment) {
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
-    try {
-        await client.connect();
-        const db = client.db("MoviesDB");
-        console.log(`Connected to database ${db.databaseName}`)
-        const movies = db.collection("Movies");
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  try {
+    await client.connect();
+    const db = client.db("MoviesDB");
+    console.log(`Connected to database ${db.databaseName}`);
+    const movies = db.collection("Movies");
 
-        // get previous comments
-        const prevComments = await movies.findOne({ "title": movie }).then(e => {
-            return e.comments
-        })
-        // concatanate previous comments and incoming comment then store in new array
-        var newComments = prevComments.concat(comment)
-        console.log(comment)
-        await movies.updateOne(
-            { "title": movie },
-            { "$set": { "comments": newComments } })
-    }
-    catch (ex) {
-        console.error(`Something bad happend ${ex}`)
-    }
-    finally {
-        client.close();
-    }
-
+    // get previous comments
+    const prevComments = await movies.findOne({ title: movie }).then((e) => {
+      return e.comments;
+    });
+    // concatanate previous comments and incoming comment then store in new array
+    var newComments = prevComments.concat(comment);
+    console.log(comment);
+    await movies.updateOne(
+      { title: movie },
+      { $set: { comments: newComments } }
+    );
+  } catch (ex) {
+    console.error(`Something bad happend ${ex}`);
+  } finally {
+    client.close();
+  }
 }
 // Return all movies as JSON
-async function returnAllMovies(movie,comment) {
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
-    try {
-        await client.connect();
-        const db = client.db("MoviesDB");
-        console.log(`Connected to database ${db.databaseName}`)
+async function returnAllMovies(movie, comment) {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  try {
+    await client.connect();
+    const db = client.db("MoviesDB");
+    console.log(`Connected to database ${db.databaseName}`);
 
-        const movies = db.collection("Movies");
-        const movieslist = await movies.find()
-        return await movieslist.toArray()
-    }
-    catch (ex) {
-        console.error(`Something bad happend ${ex}`)
-    }
-    finally {
-        client.close();
-    } 
+    const movies = db.collection("Movies");
+    const movieslist = await movies.find();
+    return await movieslist.toArray();
+  } catch (ex) {
+    console.error(`Something bad happend ${ex}`);
+  } finally {
+    client.close();
+  }
 }
 // returnAllMovies().then(e => console.log(e))
 module.exports.returnAllMovies = returnAllMovies;
 module.exports.dbSearch = dbSearch;
 module.exports.dbAddComment = dbAddComment;
 
-    // Reference 
+// Reference
 
-    // dbAddComment("The Grudge",{name:"Darius",comment:"shit finally works"})
-    // collections.forEach(c=>console.log(c));
-        //get all collections and list them out
-        // const collections = await db.collections();
+// dbAddComment("The Grudge",{name:"Darius",comment:"shit finally works"})
+// collections.forEach(c=>console.log(c));
+//get all collections and list them out
+// const collections = await db.collections();
 
-
-        // const employees = db.collection("Movies");
-        // const searchCursor = await employees.find({"title": "/A/"});
-        // const result = await searchCursor.toArray();
-        // console.log(result)
-        //console.log(await searchCursor.hasNext());
+// const employees = db.collection("Movies");
+// const searchCursor = await employees.find({"title": "/A/"});
+// const result = await searchCursor.toArray();
+// console.log(result)
+//console.log(await searchCursor.hasNext());
 /*
 while (await searchCursor.hasNext()){
     console.log(await searchCursor.next())
 }*/
 
-
-        //Insert into the collection 
+//Insert into the collection
 /*
 const insertCursor = await employees.insertMany([
     {
@@ -116,11 +110,8 @@ const updateCursor =  await employees.updateOne(
     console.log(updateCursor.modifiedCount);
     */
 
+//    const deleteCursor =  await employees.deleteOne(
+//         {"ssn": 998}
+//         )
 
-          //    const deleteCursor =  await employees.deleteOne(
-              //         {"ssn": 998}
-              //         )
-
-              //    console.log(deleteCursor.deletedCount);
-
-
+//    console.log(deleteCursor.deletedCount);
--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 // const cors = require('cors')
-const movieRouter = require('./Routes/movieRouter')
+const movieRouter = require("./Routes/movieRouter");
 
-app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 // app.use(cors() )
 
 // Main site port
-const PORT = process.env.PORT || 8000 ;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // EJS view engine
 // app.set('view engine', 'ejs')
-
 
 // middleware logging IP address
 // const logger = (req, res, next) => {
@@ -25,28 +24,27 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
 // app.use(logger)
 
-
-console.log("server coming online ...")
+console.log("server coming online ...");
 //static folder
 // app.use(express.static(path.join(__dirname, '.')));
-console.log("server online")
+console.log("server online");
 
 //request for movies page
-app.use('/movies',movieRouter)
+app.use("/movies", movieRouter);
 
 //request for home page
-app.get('/', (req, res) => {
-//   res.render('movies')
-console.log('main page requested')
-res.status(200).send('you have requested the main page')
+app.get("/", (req, res) => {
+  //   res.render('movies')
+  console.log("main page requested");
+  res.status(200).send("you have requested the main page");
 });
 //request for profile page
-app.get('/profile', (req, res) => {
-//   res.render('profile')
+app.get("/profile", (req, res) => {
+  //   res.render('profile')
 });
 
 //request for shows page
-app.get('/shows', (req, res) => {
+app.get("/shows", (req, res) => {
   // res.render('shows')
 });
 
@@ -54,5 +52,3 @@ app.get('/shows', (req, res) => {
 app.use((req, res) => {
   // res.render('404_page')
 });
-
-
